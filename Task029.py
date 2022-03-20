fc154a2c52f50a65c4bf8625bacc6451796cfa76
Cheshire Cat <Sasha-Km@yandex.ru>
2022-03-20 13:39:11 +0300

    НОК двух чисел

---
+++ Task029.py
@@ -0,0 +1,12 @@
+#29.	Найти НОК двух чисел
+print('введите первое число')
+a=int(input())
+print('введите второе число')
+b=int(input())
+#a, b = 7, 5
+i = min(a, b)
+while True:
+    if i%a==0 and i%b==0:
+        break
+    i += 1
+print('НОК равно', i)
