d1ac8fdd8c6942f70dcaf2fe2532765e6b22b9ee
Cheshire Cat <Sasha-Km@yandex.ru>
2022-03-23 21:11:18 +0300

    список из  N членов последовательности correct

--- Task011.py
+++ Task011.py
@@ -1,11 +1,18 @@
 #11.	Сформировать список из  N членов последовательности.Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
 #N=[None for _ in range(5)]
+#N=[]
+#n=1
+#N.append(n)
+# for i in range(5):
+#     n*=-3
+#     N.append(n)
+# print(N)
 N=[]
+num=int(input('введите значение '))
 n=1
-N.append(n)
-for i in range(5):
-    n*=-3
+for i in range(num):
     N.append(n)
+    n*=-3
 print(N)
 
