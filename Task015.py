c10d26a697ebf90456746d301d0b37e3de61475c
Cheshire Cat <Sasha-Km@yandex.ru>
2022-03-23 21:28:04 +0300

    набор произведений чисел от 1 до N

--- Task015.py
+++ Task015.py
@@ -1,10 +1,11 @@
 #15.	Написать программу получающую набор произведений чисел от 1 до N.
 #Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
 mult=[]
+num=int(input('введите значение '))
 n=1
-mult.append(n)
-for i in range(3):
-    n*=n+1
+
+for i in range(num):
     mult.append(n)
+    n*=n+1
 print(mult)
 
