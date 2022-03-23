13499bb02309df226363bd39e6c48141d0bf7a0e
Cheshire Cat <Sasha-Km@yandex.ru>
2022-03-23 21:18:37 +0300

    количество вхождений одной строки в другую correct

--- Task013.py
+++ Task013.py
@@ -1,14 +1,19 @@
 #13.	Пользователь задаёт две строки. Определить количество вхождений одной строки в другой
 
-firststring=input('введите первую строку  ')
-print(f"{firststring}")
-secondstring=input('введите вторую строку  ')
-print(f"{secondstring}")
+# firststring=input('введите первую строку  ')
+# print(f"{firststring}")
+# secondstring=input('введите вторую строку  ')
+# print(f"{secondstring}")
 
-firststring_count=firststring.count(secondstring)
-print(firststring_count)
-secondstring_count=secondstring.count(firststring)
-print(secondstring_count)
+# firststring_count=firststring.count(secondstring)
+# print(firststring_count)
+# secondstring_count=secondstring.count(firststring)
+# print(secondstring_count)
 
+str = input("введите первую строку: ")
+str2 = input("введите вторую строку: ")
+
+print(str2.count(str))
+print(str.count(str2))
 
 
