64ccdb22ee4582f5f921a422f3e17a7ad13514fb
Cheshire Cat <Sasha-Km@yandex.ru>
2022-03-26 15:31:31 +0300

    сумма цифр в вещественном числе корр

--- Task014.py
+++ Task014.py
@@ -5,3 +5,19 @@ for digit in n:
     if digit.isdigit():
         sum+=int(digit)
 print('Сумма= ', sum)
+
+# num=float(input('введите число '))
+# num=str(num)
+# #замена точки на пробел
+# num1=num.replace('.',' ')
+# num2=int(num1)
+# #если ввели отрицательное значение
+# if num2<0:
+#     num2*=-1
+
+# sum_didg=0
+# while num2>0:
+#     ost=num2%10
+#     sum_didg+=ost
+#     num2//=10
+# print(sum_didg)
