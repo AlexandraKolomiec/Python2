e71e57d5fd7d0d3df153748888bbacdc23c95f19
Cheshire Cat <Sasha-Km@yandex.ru>
2022-04-01 21:31:27 +0300

    список простых множителей натурального числа 2 вариант

--- Task031.py
+++ Task031.py
@@ -10,10 +10,21 @@
 
 n=int(input("введите число: "))
 list = []
-
+"""1 вариант"""
 for i in range(2,n):
     while n%i==0:
         n/=i
         list.append(i)
 print(list)
 
+
+"""2 вариант"""
+def f(n,d):
+    while d<=n:
+        if n%d==0:
+            list.append(d)
+            n//=d
+        elif n%d!=0:
+            d+=1
+f(n,2)
+print(list)            
