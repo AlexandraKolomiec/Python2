950b363ef768676b37742c995ace6147bad5a049
Cheshire Cat <Sasha-Km@yandex.ru>
2022-04-01 20:00:45 +0300

    произведение пар чисел в списке 1 и последний и тд 2 способ

--- Task023.py
+++ Task023.py
@@ -1,22 +1,27 @@
 #23.	Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-# N=[2,5,7,9,7,4]
-# i=0
-# while i<len(N):
-#     new=[N[0]*N[-1]]
-#     i+=1
-# print(new)
 
-def MultPair(listMultPair):
-    listAdd=[]
-    if (len(listMultPair)%2==0):
-        for i in range(0, len(listMultPair)//2):
-            listAdd.append(listMultPair[i]*listMultPair[len(listMultPair)-1-i])
-    else:
-        for i in range(0, (len(listMultPair)+1)//2):
-            listAdd.append(listMultPair[i]*listMultPair[len(listMultPair)-1-i])
-    return listAdd
+# def MultPair(listMultPair):
+#     listAdd=[]
+#     if (len(listMultPair)%2==0):
+#         for i in range(0, len(listMultPair)//2):
+#             listAdd.append(listMultPair[i]*listMultPair[len(listMultPair)-1-i])
+#     else:
+#         for i in range(0, (len(listMultPair)+1)//2):
+#             listAdd.append(listMultPair[i]*listMultPair[len(listMultPair)-1-i])
+#     return listAdd
 
-list1=[2, 3, 4, 5, 6]
-list2=[2, 3, 4, 5, 6, 7]
-print(MultPair(list2))       
+# list1=[2, 3, 4, 5, 6]
+# list2=[2, 3, 4, 5, 6, 7]
+# print(MultPair(list2))       
 
+import math
+lst=[2,3,4,5,6]
+res_lst=[]
+len_res_lst=math.ceil(len(lst)/2)
+temp=0
+
+for i, x in enumerate(lst):
+    if len(res_lst)!=len_res_lst:
+        temp=lst[i]*lst[-1-i]
+        res_lst.append(temp)
+print(res_lst)
