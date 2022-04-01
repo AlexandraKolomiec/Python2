041d69a167ec225bc5bb5742343a73853de07ea4
Cheshire Cat <Sasha-Km@yandex.ru>
2022-04-01 21:21:11 +0300

    большее и меньшее число в строке

--- Task027.py
+++ Task027.py
@@ -1,7 +1,18 @@
 #27.	Строка содержит набор чисел. Показать большее и меньшее число
 #Символ-разделитель - пробел
 
-str=369, 88, 17, 45, 1111
-max_number=max(str)
-min_number=min(str)
-print(max_number,' ', min_number)
+
+"""не работает на отрицательных числах"""
+input_string= input('ВВедите числа через пробел ').split(' ')
+numbers=[]
+for i in input_string:
+    if i.isdigit():numbers.append(int(i))
+
+print(f'наибольшее число = {max(numbers)}.\nнаименьшее число= {min(numbers)}')
+
+# str=369, 88, 17, 45, 1111, -3, 15, -222
+
+# max_number=int(max(str))
+# min_number=int(min(str))
+# print(max_number,' ', min_number)
+# print(type(str))
